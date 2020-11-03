df7ee2d54a3c5d11f1ec01cd9bc2ce9496dd7451
Benjamin144 <josephroberts@email.com>
2020-11-03 23:52:55 +0000

    overhaul to ux introduced carousel as front peice on after landing

--- js/main.js
+++ js/main.js
@@ -26,17 +26,63 @@ $(document).ready(function () {
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
+})
 
     // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
+    //$('.owl-carousel').owlCarousel({
+        //loop: true,
+        //autoplay: false,
+        //autoplayTimeout: 3000,
+        //dots: false,
+        //nav: true,
+        //navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        //responsive: responsive
+    //});
 
+//});
+
+$(document).ready(function () {
+  initialize_owl($('#owl1'));
+  
+  let tabs = [
+    { target: '#home', owl: '#owl1' },
+    { target: '#profile', owl: '#owl2' },
+    { target: '#messages', owl: '#owl3' },
+    { target: '#settings', owl: '#owl4' },
+  ];
+
+  // Setup 'bs.tab' event listeners for each tab
+  tabs.forEach((tab) => {
+    $(`a[href="${ tab.target }"]`)
+      .on('shown.bs.tab', () => initialize_owl($(tab.owl)))
+      .on('hide.bs.tab', () => destroy_owl($(tab.owl)));
+  });    
 });
+
+function initialize_owl(el) {
+  el.owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+}
+
+function destroy_owl(el) {
+  el.data('owlCarousel').destroy();
+}
 
