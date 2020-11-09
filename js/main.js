04e80f6fd0c85829bbf84234a5eaae092749e48c
Benjamin144 <josephroberts@email.com>
2020-11-09 20:48:38 +0000

    reverted back to a workable solution

--- js/main.js
+++ js/main.js
@@ -33,94 +33,94 @@ $(document).ready(function () {
 
 
   
-//$(document).ready(function () {
-  //initialize_owl($('#owl1'));
-    
-//});
+$(document).ready(function () {
+  initialize_owl($('#owl1'));
+   
+});
 
 
-//function showMessage() {
-  //alert( 'Hello everyone!' );
-//}
+function showMessage() {
+  alert( 'Hello everyone!' );
+}
 
 
 
-//function initialize_owl(el) {
-  //el.owlCarousel({
-    //loop: true,
-    //margin: 10,
-    //responsiveClass: true,
-    //responsive: {
-      //0: {
-        //items: 1,
-//nav: false
-      //},
-      //600: {
-        //items: 1,
-        //nav: false
-      //},
-      //1000: {
-        //items: 1,
-        //nav: false,
-        //loop: false
-      //}
-    //}
-  //});
-//}
+function initialize_owl(el) {
+  el.owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+nav: false
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: false,
+        loop: false
+      }
+    }
+  });
+}
 
 //Carousel functions//
 //*self drag away functionality on responsive carousel
 
 
-//$(document).ready(function () {
-  //initialize_owl($('#owl1'));
+$(document).ready(function () {
+  initialize_owl($('#owl1'));
 
   
-  //}); 
+  }); 
 
-//function destroy_owl(el) {
-  //el.data('owlCarousel').destroy();
-//}
-//$(document).ready(function(){
-  //const owl = $('.owl-carousel')
-  //owl.owlCarousel({
-    //loop:true,
-    //margin:5,
-    //nav:false,
-    //items: 1,
-    //dots: true
-  //});
+function destroy_owl(el) {
+  el.data('owlCarousel').destroy();
+}
+$(document).ready(function(){
+  const owl = $('.owl-carousel')
+  owl.owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    items: 1,
+    dots: true
+  });
   
   // Custom Nav directional arrows on carousel
   
-  //$('.owl-carousel__next').click(() => owl.trigger('next.owl.carousel'))
+  $('.owl-carousel__next').click(() => owl.trigger('next.owl.carousel'))
   
-  //$('.owl-carousel__prev').click(() => owl.trigger('prev.owl.carousel'))
-//})
+  $('.owl-carousel__prev').click(() => owl.trigger('prev.owl.carousel'))
+})
 
 // Get the modal
-//var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-//btn.onclick = function() {
-  //modal.style.display = "block";
-//}
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
-//span.onclick = function() {
-  //modal.style.display = "none";
-//}
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
-//window.onclick = function(event) {
-  //if (event.target == modal) {
-    //modal.style.display = "none";
-  //}
-//}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
