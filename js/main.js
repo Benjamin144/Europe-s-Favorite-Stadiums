43ca15a90e7e39214ef188859ce8d67fae5124a5
Benjamin144 <josephroberts@email.com>
2020-11-13 19:16:57 +0000

    error console clean up %90 complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+//set toggle collapse function on navbar
+
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on nav toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+})
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("mybtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
 //set responsive items across site
@@ -17,18 +60,7 @@ const responsive = {
     }
 }
 
-//set toggle collapse function on navbar
 
-$(document).ready(function () {
-
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
-
-    /** click event on nav toggle menu */
-    $toggleCollapse.click(function () {
-        $nav.toggleClass('collapse');
-    })
-})
 
 
 //customised self manual scrolling carousel
@@ -95,36 +127,6 @@ $(document).ready(function(){
   
   $('.owl-carousel__prev').click(() => owl.trigger('prev.owl.carousel'))
 })
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 
 
 
